f34c5b3fed120f247ceb3d143f61b2eb8fe89ede
VasilStamatov <vasil_0000@abv.bg>
2017-04-20 21:16:03 +0300

    Created starter scenes

---
+++ Assets/Scripts/TextureScroller.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Script to handle scrolling of textures
+/// This is used for moving background to create a parallax effect
+/// This technique offsets the texture offset of the main texture on the qued
+/// rather than moving the actual quad
+/// </summary>
+public class TextureScroller : MonoBehaviour
+{
+		//The speed for scrolling
+		public float scrollSpeed;
+
+		//the start texture offset
+		private Vector2 savedOffset;
+
+		//The material which texture will be offset
+		private Material material;
+
+		/*
+		 * Saves the current texture offset of the main texture 
+			*/
+		void Start()
+		{
+				material = GetComponent<Image>().material;
+				savedOffset = material.GetTextureOffset("_MainTex");
+		}
+
+		/*
+		 * Offsets the texture offset of the main texture to create a background scrolling effect
+			* This works when the texture setting is set to repeat
+			*/
+		void Update()
+		{
+				float x = Mathf.Repeat(Time.time * scrollSpeed, 1.0f);
+				Vector2 offset = new Vector2(x, savedOffset.y);
+				material.SetTextureOffset("_MainTex", offset);
+		}
+
+		/*
+		 * Reset the texture offset to what it originally was when the game object is being disabled
+			*/
+		void OnDisable()
+		{
+				//cannot use the private member material here, must get the component
+				GetComponent<Image>().material.SetTextureOffset("_MainTex", savedOffset);
+		}
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,5 +1,66 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+		public void PlayButtonEvent()
+		{
+				SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		}
+		public void ExitButtonEvent()
+		{
+				Application.Quit();
+		}
+		public void BackButtonEvent()
+		{
+				SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+		}
+		public void TrainingButtonEvent()
+		{
+				SceneManager.LoadScene("Training");
+		}
+		public void TournamentButtonEvent()
+		{
+				SceneManager.LoadScene("LevelSelector");
+		}
+		public void Level1Clicked()
+		{
+				SceneManager.LoadScene("Level1");
+		}
+		public void Level2Clicked()
+		{
+				SceneManager.LoadScene("Level2");
+		}
+		public void Level3Clicked()
+		{
+				SceneManager.LoadScene("Level3");
+		}
+		public void Level4Clicked()
+		{
+				SceneManager.LoadScene("Level4");
+		}
+		public void Level5Clicked()
+		{
+				SceneManager.LoadScene("Level5");
+		}
+		public void Level6Clicked()
+		{
+				SceneManager.LoadScene("Level6");
+		}
+		public void Level7Clicked()
+		{
+				SceneManager.LoadScene("Level7");
+		}
+		public void Level8Clicked()
+		{
+				SceneManager.LoadScene("Level8");
+		}
+		public void Level9Clicked()
+		{
+				SceneManager.LoadScene("Level9");
+		}
+		public void Level10Clicked()
+		{
+				SceneManager.LoadScene("Level10");
+		}
 }
