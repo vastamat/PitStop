4251e767ff008b237aba0d5d67bb8d9c8f7de667
VasilStamatov <vasil_0000@abv.bg>
2017-04-24 16:56:41 +0300

    Added checkpoints on the track and lap tracking/counter

---
+++ Assets/Scripts/CarLapCounter.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CarLapCounter : MonoBehaviour
+{
+		public TrackCheckpoint m_first;
+		public Text m_lapsText;
+
+		private TrackCheckpoint m_next;
+		private int m_numLaps;
+
+		void Start()
+		{
+				m_numLaps = 0;
+				SetNextCheckpoint(m_first);
+				UpdateText();
+		}
+
+		public void OnReachCheckpoint(TrackCheckpoint _checkpoint)
+		{
+				if (_checkpoint == m_next)
+				{
+						if (m_first == m_next)
+						{
+								m_numLaps++;
+								UpdateText();
+						}
+						SetNextCheckpoint(_checkpoint);
+				}
+		}
+
+		public TrackCheckpoint GetNextCheckpoint()
+		{
+				return m_next;
+		}
+
+		private void UpdateText()
+		{
+				if (m_lapsText)
+				{
+						m_lapsText.text = "Laps: " + m_numLaps.ToString();
+				}
+		}
+
+		private void SetNextCheckpoint(TrackCheckpoint _checkpoint)
+		{
+				m_next = _checkpoint.m_next;
+		}
+}
---
+++ Assets/Scripts/TrackCheckpoint.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Class for the checkpoints on the tracks
+/// which upon being reached call the callback functions of the cars
+/// </summary>
+[RequireComponent(typeof(BoxCollider2D))]
+public class TrackCheckpoint : MonoBehaviour
+{
+		public TrackCheckpoint m_next;
+
+		void OnTriggerEnter2D(Collider2D _other)
+		{
+				CarLapCounter clc = _other.GetComponent<CarLapCounter>();
+				if (clc)
+				{
+						clc.OnReachCheckpoint(this);
+				}
+		}
+}
