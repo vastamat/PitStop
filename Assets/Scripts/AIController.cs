31b578ac569e7a144c655197d01ae6b29b1f23b9
VasilStamatov <vasil_0000@abv.bg>
2017-04-24 16:56:02 +0300

    Added an AI controller (WIP)

---
+++ Assets/Scripts/AIController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Let artificial intelligence control the car
+/// </summary>
+public class AIController : Car2DController
+{
+		void FixedUpdate()
+		{
+				Accelerate();
+
+				TrackCheckpoint target = GetCurrentTarget();
+
+				Debug.Log(target);
+
+				Vector3 towardsNextCheckpoint = transform.position - target.transform.position;
+				float angleBetweenCP = Vector2.Angle(Vector2.up, towardsNextCheckpoint.normalized) * Mathf.Sign(towardsNextCheckpoint.y);
+				float rot = Mathf.MoveTowardsAngle(transform.localEulerAngles.z, angleBetweenCP, 0.3f);
+				transform.eulerAngles = new Vector3(0.0f, 0.0f, rot);
+				//Steer(Mathf.Sign(angleBetweenCP));
+		}
+
+		private TrackCheckpoint GetCurrentTarget()
+		{
+				CarLapCounter clc = GetComponent<CarLapCounter>();
+				if (clc)
+				{
+						return clc.GetNextCheckpoint();
+				}
+				return null;
+		}
+}
